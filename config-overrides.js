9daa49b8e44aa32e980736edd5064cb658abc586
Nitin Ragavan <nitin.r21@gmail.com>
2025-03-08 22:59:22 -0500

    Fix Webpack path-browserify polyfill

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const { override, addWebpackAlias } = require("customize-cra");
-
-module.exports = override(
-  addWebpackAlias({
-    "path": require.resolve("path-browserify"),  // ✅ Fix Webpack resolution
-  })
-);
