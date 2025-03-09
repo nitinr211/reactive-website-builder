057173b3adba8e16d2ac19a956ce09bf68b66acf
Nitin Ragavan <nitin.r21@gmail.com>
2025-03-08 23:27:49 -0500

    Disable Webpack path polyfill and update build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const webpack = require("webpack");
 
 module.exports = {
-  resolve: {
-    fallback: {
-      "path": false  // ✅ Disables Webpack from trying to polyfill 'path'
-    }
-  }
+  target:'node',
+  
 };
